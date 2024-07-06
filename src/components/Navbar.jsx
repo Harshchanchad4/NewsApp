666b699719b00fbc361acbbdf8b2ae3e46f5d74e
Harshchanchad4 <150885764+Harshchanchad4@users.noreply.github.com>
2024-07-06 18:56:10 +0530

    add necessary changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,33 +3,36 @@ import news from "/assets/news.png"
 import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
-        <div className='h-16 w-full bg-blue-950 flex justify-center items-center gap-56'>
-            <Link to="/" >
-                <img src={news} alt="Studynotion logo" width={60} height={60} loading='lazy' />
-            </Link>
+        <div className='w-full h-16 bg-blue-950 flex justify-center items-center'>
 
-            <ul className='text-blue-300 flex justify-center items-center font-semibold gap-6'>
-                <Link to={"/"}>
-                    <li className='hover:text-blue-100'>Home</li>
-                </Link>
-                <Link to={"/about"}>
-                    <li className='hover:text-blue-100'>About us</li>
-                </Link>
-                <Link to={"/contact"}>
-                    <li className='hover:text-blue-100'>Contact us</li>
+            <div className='h-full w-[1080px] bg-blue-950 flex  items-center gap-56 ml-40'>
+                <Link to="/" >
+                    <img src={news} alt="Studynotion logo" width={60} height={60} loading='lazy' />
                 </Link>
 
-            </ul>
+                <ul className='text-blue-300 flex justify-center items-center font-semibold gap-20 ml-30'>
+                    <Link to={"/"}>
+                        <li className='hover:text-blue-100'>Home</li>
+                    </Link>
+                    <Link to={"/about"}>
+                        <li className='hover:text-blue-100'>About us</li>
+                    </Link>
+                    <Link to={"/contact"}>
+                        <li className='hover:text-blue-100'>Contact us</li>
+                    </Link>
 
-            {/* <div className='text-blue-300 flex justify-center items-center font-semibold gap-4'>
+                </ul>
+
+                {/* <div className='text-blue-300 flex justify-center items-center font-semibold gap-4'>
 
                 <button className='bg-gray-700 rounded-md p-2 hover:transition-all duration-200  hover:scale-95 hover:bg-blue-300 hover:text-black'>
-                    Log In
+                Log In
                 </button>
                 <button className='bg-gray-700 rounded-md p-2 hover:transition-all duration-200  hover:scale-95 hover:bg-blue-300 hover:text-black'>
-                    Sign Up
+                Sign Up
                 </button>
-            </div> */}
+                </div> */}
+            </div>
         </div>
 
     )
